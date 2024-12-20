bd23ff08b59bf2bb750eb5e9a2062b1e53d0bcfe
Clarence Kieth M. Dela Cruz <clarencekiethdelacruz@gmail.com>
2024-12-20 17:41:42 +0800

    Save data and generate HTML for paymentSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,10 @@ import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import {
+  deliveryOptions,
+  getDeliveryOption,
+} from "../../data/deliveryOptions.js";
 hello();
 
 const today = dayjs();
@@ -18,13 +21,7 @@ export function renderOrderSummary() {
 
     const deliveryOptionsId = cartItem.deliveryOptionsId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionsId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionsId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
